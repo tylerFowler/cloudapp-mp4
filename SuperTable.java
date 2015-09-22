5737a6a78286475f09d3c25a79fe85731ca15547
Tyler Fowler <tylerfowler.1337@gmail.com>
2015-09-21 23:04:15 -0500

    Added implementation of MP4

--- SuperTable.java
+++ SuperTable.java
@@ -25,21 +25,21 @@ public class SuperTable{
       Configuration config = HBaseConfiguration.create();
 
       // Instaniate HBaseAdmin class
-      HBaseAdmin admin = new HBaseAdmin(conf);
+      HBaseAdmin admin = new HBaseAdmin(config);
 
       // Instantiate table descriptor class
-      HTableDescriptor[] tableDescriptor = admin.listTables();
-
-      // Print all table names
-      for (int i = 0; i < tableDescriptor.length; i++) {
-        System.out.println(tableDescriptor[i].getNameAsString());
-      }
+      HTableDescriptor tableDescriptor = new HTableDescriptor(TableName.valueOf("powers"));
 
       // Add column families to table descriptor
+      tableDescriptor.addFamily(new HColumnDescriptor("personal"));
+      tableDescriptor.addFamily(new HColumnDescriptor("professional"));
 
       // Execute the table through admin
+      admin.createTable(tableDescriptor);
+      System.out.println("Table Created");
 
       // Instantiating HTable class
+      HTable hTable = new HTable(config, "powers");
 
       // Repeat these steps as many times as necessary
 
@@ -49,21 +49,51 @@ public class SuperTable{
       	      // Add values using add() method
               // Hints: Accepts column family name, qualifier/row name ,value
 
+      Put superman = new Put(Bytes.toBytes("row1"));
+
+      superman.add(Bytes.toBytes("personal"), Bytes.toBytes("hero"), Bytes.toBytes("superman"));
+      superman.add(Bytes.toBytes("personal"), Bytes.toBytes("power"), Bytes.toBytes("strength"));
+      superman.add(Bytes.toBytes("professional"), Bytes.toBytes("name"), Bytes.toBytes("clark"));
+      superman.add(Bytes.toBytes("professional"), Bytes.toBytes("xp"), Bytes.toBytes("100"));
+
+      Put batman = new Put(Bytes.toBytes("row2"));
+      batman.add(Bytes.toBytes("personal"), Bytes.toBytes("hero"), Bytes.toBytes("batman"));
+      batman.add(Bytes.toBytes("personal"), Bytes.toBytes("power"), Bytes.toBytes("money"));
+      batman.add(Bytes.toBytes("professional"), Bytes.toBytes("name"), Bytes.toBytes("bruce"));
+      batman.add(Bytes.toBytes("professional"), Bytes.toBytes("xp"), Bytes.toBytes("50"));
+
+      Put wolverine = new Put(Bytes.toBytes("row3"));
+      wolverine.add(Bytes.toBytes("personal"), Bytes.toBytes("hero"), Bytes.toBytes("wolverine"));
+      wolverine.add(Bytes.toBytes("personal"), Bytes.toBytes("power"), Bytes.toBytes("healing"));
+      wolverine.add(Bytes.toBytes("professional"), Bytes.toBytes("name"), Bytes.toBytes("logan"));
+      wolverine.add(Bytes.toBytes("professional"), Bytes.toBytes("xp"), Bytes.toBytes("75"));
+
       // Save the table
+      hTable.put(superman);
+      hTable.put(batman);
+      hTable.put(wolverine);
 
       // Close table
+      hTable.close();
 
       // Instantiate the Scan class
+      Scan scan = new Scan();
 
       // Scan the required columns
+      scan.addColumn(Bytes.toBytes("personal"), Bytes.toBytes("hero"));
 
       // Get the scan result
+      ResultScanner scanner = hTable.getScanner(scan);
 
       // Read values from scan result
       // Print scan result
+      for (Result result = scanner.next(); result != null; result = scanner.next())
+        System.out.println("Got row: " + result);
 
       // Close the scanner
+      scanner.close();
 
       // Htable closer
+      hTable.close();
    }
 }
